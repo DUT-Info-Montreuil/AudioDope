cca8cb3bef97cf03fe0044e1a94978b880764af3
Steven YANG <steven.yang.ys@gmail.com>
2022-11-28 21:34:59 +0100

    ajout recherche par description

---
+++ modules/mod_recherche/cont_recherche.php
@@ -0,0 +1,63 @@
+<?php
+    if (constant("lala") != "layn")
+        die("wrong constant");
+
+    include_once('modele_recherche.php');
+    include_once('vue_recherche.php');
+    
+    class ContRecherche {
+
+        private $modele;
+        private $vue;
+        private $action;
+
+        public function __construct() {
+            $this->modele = new ModeleRecherche();
+            $this->vue = new VueRecherche();
+            $this->action = isset($_GET['action']) ? $_GET['action'] : "";
+        }
+
+        public function exec() {
+            $this->vue->menu();
+            $this->vue->indic_section($this->action);
+            switch($this->action) {
+                case 'section_tout' : 
+                    $this->recherche();
+                    break;
+                case 'section_tag' : 
+                    $this->recherche_par_tag();
+                    break;
+                case 'section_titre' : 
+                    $this->recherche_par_titre();
+                    break;
+                case 'section_desc' : 
+                    $this->recherche_par_description();
+                    break;
+                case 'section_user' : 
+                    $this->recherche_par_user();
+                    break;
+                default : die("action inexistant");
+            }
+            $this->vue->affichage();
+        } 
+
+        public function recherche() {
+            $this->vue->affiche_posts($this->modele->recherche());
+        }
+
+        public function recherche_par_tag() {
+            $this->vue->affiche_posts($this->modele->recherche_par_tag());
+        }
+
+        public function recherche_par_titre() {
+            $this->vue->affiche_posts($this->modele->recherche_par_titre());
+        }
+
+        public function recherche_par_description() {
+            $this->vue->affiche_posts($this->modele->recherche_par_description());
+        }
+
+        public function recherche_par_user() {
+            $this->vue->affiche_posts($this->modele->recherche_par_user());
+        }
+    }
