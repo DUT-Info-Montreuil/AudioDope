56aa6230b4308b96e233a5d71fdd71cfe19e49ed
G8LD <steven.yang.ys@gmail.com>
2022-12-02 10:45:27 +0100

    Revert "optimisation aimer_tags"
    This reverts commit 635b77c6a37d03f1805a157d566a60f395c78187.

--- template.php
+++ template.php
@@ -11,7 +11,7 @@ if (constant("lala") != "layn")
 	<META CHARSET="UTF-8" />
 	<TITLE>AudioDope</TITLE>
 	<LINK href="style.css" rel="stylesheet" type="text/css">
-	<link rel="icon" type="image/png" href="ressources/icons/logo.png">
+	<link rel="shortcut icon" href="#">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </HEAD>
 
